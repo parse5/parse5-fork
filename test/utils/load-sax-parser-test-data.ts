a1e45e9dd599beabed4f25b0d8098d83d0b7a63c
Felix Böhm <188768+fb55@users.noreply.github.com>
2021-11-25 16:36:48 +0000

    test: Add return types

--- test/utils/load-sax-parser-test-data.ts
+++ test/utils/load-sax-parser-test-data.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { normalizeNewLine } from './common.js';
 
-export function loadSAXParserTestData() {
+export function loadSAXParserTestData(): { name: string; src: string; expected: string }[] {
     const dataDirPath = new URL('../data/sax', import.meta.url);
     const testSetFileDirs = fs.readdirSync(dataDirPath);
 
